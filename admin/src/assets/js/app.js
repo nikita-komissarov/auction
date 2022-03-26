aeaca3ad473260fd56ce261b3097fe8ee3067954
Nikita Komissarov <48474884+nikita-komissarov@users.noreply.github.com>
2022-03-26 22:44:41 +0300

    Создан каркас F7

---
+++ admin/src/assets/js/app.js
@@ -0,0 +1,25 @@
+// Dom7
+var $ = Dom7;
+
+// Init App
+var app = new Framework7({
+  id: 'io.framework7.testapp',
+  el: '#app',
+  theme: 'ios',
+  // store.js,
+  store: store,
+  // routes.js,
+  routes: routes,
+  popup: {
+    closeOnEscape: true,
+  },
+  sheet: {
+    closeOnEscape: true,
+  },
+  popover: {
+    closeOnEscape: true,
+  },
+  actions: {
+    closeOnEscape: true,
+  },
+});
