96d54100f7cbec62cd89032d7ca63bf32073c80b
Nikita Komissarov <48474884+nikita-komissarov@users.noreply.github.com>
2022-03-27 23:15:46 +0300

    Создана страница создания лота

---
+++ admin/src/pages/home.php
@@ -0,0 +1,28 @@
+<template>
+  <div class="page page-home">
+    <div class="navbar navbar-transparent">
+      <div class="navbar-bg"></div>
+      <div class="navbar-inner">
+        <div class="left">
+          <a href="#" class="link icon-only panel-open" data-panel="left">
+            <i class="icon f7-icons if-not-md">menu</i>
+            <i class="icon material-icons md-only">menu</i>
+          </a>
+        </div>
+        <div class="title sliding">Framework7</div>
+        <div class="right">
+          <a class="link icon-only searchbar-enable" data-searchbar=".searchbar-components">
+            <i class="icon f7-icons if-not-md">search</i>
+            <i class="icon material-icons md-only">search</i>
+          </a>
+        </div>
+        <div class="title-large">
+          <div class="title-large-text">Framework7</div>
+        </div>
+      </div>
+    </div>
+    <div class="page-content">
+      <div class="block-title block-title-medium searchbar-found">Components</div>
+    </div>
+  </div>
+</template>
