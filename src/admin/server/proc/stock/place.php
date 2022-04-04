025fc3a3303e66839a05bfa5350e9e28c9b1da24
Nikita Komissarov <48474884+nikita-komissarov@users.noreply.github.com>
2022-04-05 00:26:19 +0300

    Добавлено указание стеллажа и ячейки для товара на складе

--- src/admin/server/proc/stock/place.php
+++ src/admin/server/proc/stock/place.php
@@ -3,41 +3,32 @@
   if(!isset($_POST['item']) || empty($_POST['item']) || !is_numeric($_POST['item'])) exit();
   if(!isset($_POST['stock']) || empty($_POST['stock']) || !is_numeric($_POST['stock'])) exit();
   if(!isset($_POST['rack'])) exit();
-  if(!isset($_POST['cell']) || empty($_POST['cell']) || !is_numeric($_POST['cell'])) exit();
+  if(!isset($_POST['cell'])) exit();
 
   require_once $_SERVER['DOCUMENT_ROOT'].'/server/server.php';
 
   //if(!$admin) exit();
 
-  $item = R::findOne('items', 'id = ?', [$_POST['id']]);
-  $stocks = R::findAll('stock');
-  $category = R::findOne('items__categories', 'id = ?', [$item->category_id]);
+  $item = R::findOne('items', 'id = ?', [$_POST['item']]);
+  if(!$item) exit();
+  $stock = R::findOne('stock', 'id = ?', [$_POST['stock']]);
+  if(!$stock) exit();
 
-  $result = [
-    'info' => [
-      'article' => $item->article,
-      'category' => [
-        'id' => $category->id,
-        'name' => $category->name,
-      ],
-      'name' => $item->name,
-      'media' => $item->media,
-      'market' => $item->market,
-      'description' => $item->description,
-      'price' => $item->price,
-    ],
-    'stocks' => [],
-  ];
-
-  foreach($stocks as $key => $stock) {
-    $result['stocks'][] = [
-      'id' => $stock->id,
-      'name' => $stock->name,
-      'address' => $stock->address,
-      'count' => 0,
-    ];
+  $stock_item = R::findOne('stock__items', 'stock_id = ? AND item_id = ?', [$_POST['stock'], $_POST['item']]);
+  if(!$stock_item){
+    $stock_item = R::xdispense('stock__items');
+    $stock_item->stock_id = $_POST['stock'];
+    $stock_item->item_id = $_POST['item'];
+    $stock_item->count = 0;
+    $stock_item->rack = 'AAAA';
+    $stock_item->cell = 0;
   }
 
-  exit(json_encode($result));
+  if($_POST['rack']) $stock_item->rack = $_POST['rack'];
+  else $stock_item->cell = $_POST['cell'];
+
+  R::store($stock_item);
+
+  exit(json_encode('ok'));
 
 ?>
