3a87f40a8a5cef357402366f48aa79c931928a83
Nikita Komissarov <48474884+nikita-komissarov@users.noreply.github.com>
2022-04-05 00:26:55 +0300

    Добавлены данные в объект товара

--- src/admin/server/proc/stock/item.php
+++ src/admin/server/proc/stock/item.php
@@ -10,28 +10,35 @@
   $stocks = R::findAll('stock');
   $category = R::findOne('items__categories', 'id = ?', [$item->category_id]);
 
+  $instance = stream_socket_client('tcp://127.0.0.1:8000');
+  fwrite($instance, 'send');
+
   $result = [
     'info' => [
       'article' => $item->article,
       'category' => [
-        'id' => $category->id,
+        'id' => (int)$category->id,
         'name' => $category->name,
       ],
       'name' => $item->name,
       'media' => $item->media,
       'market' => $item->market,
       'description' => $item->description,
-      'price' => $item->price,
+      'price' => (float)$item->price,
     ],
     'stocks' => [],
   ];
 
   foreach($stocks as $key => $stock) {
+    $stock_item = R::findOne('stock__items', 'stock_id = ? AND item_id = ?', [$stock->id, $item->id]);
     $result['stocks'][] = [
-      'id' => $stock->id,
+      'id' => (int)$stock->id,
       'name' => $stock->name,
       'address' => $stock->address,
-      'count' => 0,
+      //Сущность склада
+      'count' => (int)$stock_item->count ?: 0,
+      'rack' => $stock_item->rack ?: 'AAAA',
+      'cell' => (int)$stock_item->cell ?: 0,
     ];
   }
 
