85b6f32e6880f85f783d27a38ce32d20a076b010
Nikita Komissarov <48474884+nikita-komissarov@users.noreply.github.com>
2022-04-05 00:27:49 +0300

    Реализовано подключение и общение с websocket

--- src/admin/assets/js/app.js
+++ src/admin/assets/js/app.js
@@ -27,6 +27,25 @@ var app = new Framework7({
   },
 });
 
+var ws;
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if(parts.length === 2) return parts.pop().split(';').shift();
+}
+
+window.onload = function () {
+  ws = new WebSocket('ws://localhost:7777?key=' + getCookie('AUCTION-ADMIN-ID'));
+  ws.onmessage = function(message) {
+    let data = JSON.parse(JSON.parse(message.data.trim()));
+    console.log("data", data);
+    if(data.cmd = 'change stock count'){
+      $(document).find('#stocks-item-' + data.item_id).find('#stock-' + data.stock_id).find('.stock-count').val(data.count);
+    }
+  };
+};
+
 var scannerFocusEl = null;
 $(document).keydown(function(event) {
   if(event.which == 113) {
