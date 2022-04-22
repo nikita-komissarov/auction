06b735c5ddb08a0816ed4aca3129aa92287775a3
Nikita Komissarov <48474884+nikita-komissarov@users.noreply.github.com>
2022-04-22 03:18:22 +0300

    Доработано хранилище на отрисовку индикаторов меню

--- src/admin/assets/js/store.js
+++ src/admin/assets/js/store.js
@@ -1,3 +1,28 @@
+function setMenuCount(el, color, value, showNull = false){
+  console.log("el", el);
+  console.log("value", value);
+  if(!value && !showNull) el.innerHTML = '';
+  else el.innerHTML = '<span class="badge color-' + color + '">' + value + '</span>';
+  return true;
+  //
+}
+
+function storeMenuCountPhotoDesc(items){
+  console.log("items", items);
+
+  let countPhoto = 0;
+  let countDescription = 0;
+
+  items.forEach(function(item){
+    if(!item.info.media.length) countPhoto++;
+    if(!item.info.description) countDescription++;
+  });
+  return {
+    photo: countPhoto,
+    description: countDescription,
+  };
+}
+
 var store = Framework7.createStore({
   state: {
     items: [],
@@ -26,7 +51,12 @@ var store = Framework7.createStore({
       .then((items) => {
         state.items = items;
         state.loaded = true;
+
+        let count = storeMenuCountPhotoDesc(items);
+        setMenuCount(document.getElementById('menu-count-photo'), 'red', count.photo);
+        setMenuCount(document.getElementById('menu-count-description'), 'red', count.description);
       });
+
     },
     reloadItem({ state }, { item_id }) {
       fetch('/server/proc/stock/items.php?id=' + item_id)
@@ -34,10 +64,18 @@ var store = Framework7.createStore({
       .then((item) => {
         //Записываем текущий массив временно
         let tmpItems = state.items;
+        //Получаем индекс этого товара в массиве по параметру ID
+        let index = tmpItems.findIndex(el => el.id == item_id);
+        //Создаём новый элемент если позиция новая
+        if(index == -1) tmpItems.push(item[0]);
         //Заменяем найдя нужный индекс на данные пришедшие от сервера
-        tmpItems[tmpItems.findIndex(el => el.id == item_id)] = item[0];
+        else tmpItems[index] = item[0];
         //Вписываем отредактированный массив назад для поддержки реактивности
         state.items = tmpItems;
+
+        let count = storeMenuCountPhotoDesc(state.items);
+        setMenuCount(document.getElementById('menu-count-photo'), 'red', count.photo);
+        setMenuCount(document.getElementById('menu-count-description'), 'red', count.description);
       });
     },
   },
