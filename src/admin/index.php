bc830098c51c606ad3d73224738cd6f491fec670
Nikita Komissarov <48474884+nikita-komissarov@users.noreply.github.com>
2022-04-26 13:56:49 +0300

    Смена языка

--- src/admin/index.php
+++ src/admin/index.php
@@ -4,7 +4,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru-RU">
 
 <head>
   <meta charset="UTF-8">
@@ -185,7 +185,7 @@
         </div>
       </div>
     </div>
-    <div class="view view-main view-init safe-areas" data-master-detail-breakpoint="768" data-url="/stock/create/"></div>
+    <div class="view view-main view-init safe-areas" data-master-detail-breakpoint="768" data-url="/stock/item/2/"></div>
     <?php else : ?>
     <div class="view view-main view-init safe-areas" data-master-detail-breakpoint="768" data-url="/auth/"></div>
     <?php endif; ?>
